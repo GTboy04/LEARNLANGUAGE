5d12bdf55d7c05cfe0ed8051997e9d3cdfc193bc
gtboy <gtboy@gt.com>
2024-02-26 19:00:10 +0800

    2024 - 2-26java

---
+++ 安徽新华学院专业课代码/java/Demo01.java
@@ -0,0 +1,12 @@
+public class Demo01{
+	public static void main(String args[]){
+		String ID = "2242159106";
+		String name = "GTboy";
+		int age = 20;
+		String tel = "18956077777";
+		System.out.println("ID:"+ID);
+		System.out.println("name:"+name);
+		System.out.println("age:"+age);
+		System.out.println("tel:"+tel);
+	}
+}
